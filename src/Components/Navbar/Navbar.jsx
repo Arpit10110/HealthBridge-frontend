8acbb3757bf89b96e6dbf563a77898f4b40c0b59
Arpit <omagrahari55@gmail.com>
2024-07-10 23:26:48 +0530

    mobile version is 80% done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,76 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from "react";
+import "./Navbar.css";
 //imports
-import { Link } from 'react-router-dom'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import {useSelector} from "react-redux"
+import { Link } from "react-router-dom";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useSelector } from "react-redux";
+import Drawer from "@mui/material/Drawer";
+import MenuIcon from "@mui/icons-material/Menu";
 //image
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.png";
 const Navbar = () => {
-  const { User_id } = useSelector(state => state.healtbrdigedeatil);
-  const { cart } = useSelector(state => state.healtbrdigedeatil);
+  const [open, setOpen] = useState(false);
+  const { User_id } = useSelector((state) => state.healtbrdigedeatil);
+  const { cart } = useSelector((state) => state.healtbrdigedeatil);
   return (
     <nav>
-        <div className="logo">
-            <img src={logo} alt=" " />
-            <h2><span>Health</span>Bridge</h2>
-        </div>
-        <div className="nav-links">
-            <Link to="/" >Home</Link>
-            {
-              User_id==""?"":<Link to="/myorders">My Orders</Link>
-            }
-            <Link to="/service" >Service</Link>
-            <Link to="/cart" ><ShoppingCartIcon  className='Cart-Icon'/><span>{cart.length}</span></Link>
-            <Link to="/smartai" >Smart AI</Link>
-            {
-            User_id==""?<Link to="/signup">SignUp</Link>:""
-            }
-            {
-            User_id==""?<Link to="/login">LogIn</Link>:<Link to="/profile">Profile</Link>
-            }
-        </div>
+      <div className="logo">
+        <img src={logo} alt=" " />
+        <h2>
+          <span>Health</span>Bridge
+        </h2>
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        {User_id == "" ? "" : <Link to="/myorders">My Orders</Link>}
+        <Link to="/service">Service</Link>
+        <Link to="/cart">
+          <ShoppingCartIcon className="Cart-Icon" />
+          <span>{cart.length}</span>
+        </Link>
+        <Link to="/smartai">Smart AI</Link>
+        {User_id == "" ? <Link to="/signup">SignUp</Link> : ""}
+        {User_id == "" ? (
+          <Link to="/login">LogIn</Link>
+        ) : (
+          <Link to="/profile">Profile</Link>
+        )}
+      </div>
+      <div className="mobile-links">
+        <button
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          <MenuIcon className="MenuIcon" />
+        </button>
+        <Drawer
+          anchor={"right"}
+          open={open}
+          onClose={() => {
+            setOpen(false);
+          }}
+        >
+          <div className="all-links">
+          <Link to="/">Home</Link>
+        {User_id == "" ? "" : <Link to="/myorders">My Orders</Link>}
+        <Link to="/service">Service</Link>
+        <Link to="/cart">
+          <ShoppingCartIcon className="Cart-Icon" />
+          <span>{cart.length}</span>
+        </Link>
+        <Link to="/smartai">Smart AI</Link>
+        {User_id == "" ? <Link to="/signup">SignUp</Link> : ""}
+        {User_id == "" ? (
+          <Link to="/login">LogIn</Link>
+        ) : (
+          <Link to="/profile">Profile</Link>
+        )}
+          </div>
+        </Drawer>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
